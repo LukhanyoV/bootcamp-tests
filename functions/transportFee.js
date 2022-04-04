d6a2009627da39de138c2fa9c05bb7ff4aea1262
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-04-04 18:45:15 +0200

    Added transportFee function and test

---
+++ functions/transportFee.js
@@ -0,0 +1,12 @@
+const transportFee = (when) => {
+  	switch(when){
+      case "morning":
+        return "R20";
+      case "afternoon":
+        return "R10";
+      case "nightshift":
+        return "free";
+      default:
+        return when + " is not a shift";
+   	}
+}
