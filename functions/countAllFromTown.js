499d08ce4ed1e809d34f2e30dfbc407392c057ec
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-04-04 17:11:13 +0200

    Added countAllFromTown function and test

---
+++ functions/countAllFromTown.js
@@ -0,0 +1,11 @@
+const countAllFromTown = (string, regCode) => {
+	let list = string.split(",");
+    let count = 0;
+  	let i = 0;
+  	while(i < list.length){
+      	let item = list[i].trim();
+      	if(item.startsWith(regCode)) count++;
+        i++;
+    }
+  	return count;
+}
