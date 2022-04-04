418a78690b15011e2a2c407a671a554cf6475ba4
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-04-04 19:32:49 +0200

    Added findItemsOver function and test

---
+++ functions/findItemsOver.js
@@ -0,0 +1,8 @@
+const findItemsOver = (list, quantity) => {
+  	let over = [];
+  	for(let i = 0; i < list.length; i++){
+      	let obj = list[i];
+      	if(obj["qty"] > quantity) over.push(obj);
+    }
+  	return over;
+}
