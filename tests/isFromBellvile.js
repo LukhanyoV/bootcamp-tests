d8c8a40a704c81992232a1070cf6e02ba84bc06c
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2022-04-04 15:43:13 +0200

    Moved isFromBellvile test to own file

---
+++ tests/isFromBellvile.js
@@ -0,0 +1,14 @@
+// Testing my isFromBellvile function
+describe('My isFromBellvile function', () => {
+    it('should return true since the plates number starts with \'CY\'', () => {
+        let results = isFromBellville("CY 123");
+        let expected = true;
+        assert.equal(results, expected);
+    });
+
+    it('should return false since the plate number does not start with \'CY\'', () => {
+        let results = isFromBellville("CJ 123");
+        let expected = false;
+        assert.equal(results, expected);
+    });
+});
